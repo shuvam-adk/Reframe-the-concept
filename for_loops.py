79d3e64036c2a262d9cf76ebf52f1074be40ef97
Shuvam Adhikkari <adhikarishuvam27@gmail.com>
2025-12-02 23:19:14 +0545

    Add files via upload

---
+++ for_loops.py
@@ -0,0 +1,318 @@
+###..92.........Loop Through a List      You can loop through the list items by using a for loop:
+
+##...ForLoop...
+
+
+
+
+#..syntax = for i in range():
+
+for i in range (0,5):
+    print(i)
+    
+    
+##..
+for i in range(0,5):
+    print(i)    
+    
+#..
+name="shuva"
+for i in name:    #..Rules ko anushar chai range hunu parne thyo but variable huna le yo ma variable ko name halne ho
+    print(i)
+
+
+##..
+name="shuvam"
+for i in name:
+    print(i) 
+
+
+###..
+car="mustang"
+for i in car:
+    print(i)
+
+
+##...
+brand="skurt"
+for i in brand:
+    print(i)
+
+
+##..
+dog="golden"
+for i in dog:
+    print(i)
+
+
+#..#
+name=["kriti","suhvam"]
+for i in name:
+    print(i)
+
+
+##..
+brand=["guchi","lui","pradao"]
+for i in brand:
+    print(i)
+
+##..
+bar="aar","car","thar"
+for i in bar:
+    print(i)
+
+
+##..
+names_of_the_students=["shuvam","raj","bikal","nayan"]
+for i in names_of_the_students:
+    print(i)
+
+
+##..
+boat="coal","thouat","load"
+for i in boat:
+    print(i)
+    
+
+
+#..
+
+name=["shuvam","kriti","sajan","wander","women"]
+for i in name:
+    print(i)
+
+
+
+##...
+name=["shuvam","riti","babu"]
+for i in range(len(name)):
+  print(name[i])
+
+
+##..
+var=["shuvam","raj","bak"]
+for i in  range(len(var)):
+    print(var[i])
+
+
+##..
+value=["any","thing","is ","ok"]
+for i in  range(len(value)):
+    print(value[i])
+    
+    
+##..
+name=["nana","baba","mama"]
+for i in range(len(name)):
+    print(name[i])
+
+
+#.....
+name=["raj","arj","khaj"]
+for i in range(len(name)):
+    print(name[i])
+    
+    
+#..
+my_list=["aa","ss","cc"]
+for i in range(len(my_list)):
+    print(my_list[i])
+    
+
+##..
+name=["shuvam","krtiti"]
+for i in range(len(name)):
+    print(name[i])
+    
+
+
+##
+my_name=["shu","saahu","raju"]
+for  i in range(len(my_name)):
+    print(my_name[i]) 
+
+
+
+#..
+nam=["shuvam","sajan","sampurna","rag"]
+for i in range(len(nam)):
+    print(nam[i])
+    
+    
+##...
+value=["bike","car","byecycle"]
+for i in  value:
+    print(i) 
+
+
+##..
+name=["shuvam","dodo","jojo"]
+for i in range(len(name)):
+    print(name[i])
+
+
+    
+    
+##..
+v="shuva","raj","panda"
+for i in v:
+    print(i)
+    
+
+
+##..
+for i in range(1,9):
+    print(i)
+
+##..
+for i in ("shuvam"):
+    print(i)
+
+##..
+for i in ("raja ko xora"):
+    print(i)
+    
+##..
+for i in ("karup"):
+    print(i)
+
+
+###..
+for i in (1,10):
+    print(i)
+
+##
+for i in ("shuva"):
+    print(i)
+
+
+
+##..
+for i in range(1,6):
+    print(i)
+    
+    
+##..
+
+for i in range(1,5):
+    print(i)
+
+
+
+##..
+name="shuvam"
+for i in name:
+    print(i)
+
+
+#..
+name="shuvam"
+for i in name:
+    print(i)
+
+
+##..
+for i in "shuvam":
+    print(i)
+    
+    
+    
+    
+# ##...
+
+name="jadu"
+for  i in range(len(name)):
+    print(name[i])
+
+
+##..
+for i in range (1,6):
+    print(i) 
+
+
+
+#....
+num=(1,2,3,4)
+for i in (range(len(num))):
+    print(i)
+
+
+
+
+#...
+for i in reversed(range(1,6)):         ##.. reversed function le raverce garch a output lai ....
+    print(i)
+
+
+##..
+for i in  reversed(range(1,9)):
+    print(i)
+
+
+###...
+for i in reversed("shuvam"):
+    print(i)
+
+
+##..
+for i in reversed("raja"):
+    print(i)
+
+
+##..
+value="kriti"
+for i in reversed(value):
+    print(i)
+
+
+
+##..
+credit_card="11-22-1-1"
+for i in reversed(credit_card):
+    print(i)
+
+##..
+name=[1,2,3,4,5,6,7]
+for i in  reversed(name):
+    print(i)
+
+###...
+
+name=("shuvam","kriti")
+for i in (name):
+    print(i)
+
+##..
+name="suvam","kara"
+for i in (name):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
